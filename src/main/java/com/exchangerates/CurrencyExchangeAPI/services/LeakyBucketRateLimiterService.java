c8c73b2bf1b1fb1357e06f8ad41cc7a6aaa3ef56
notjoao1 <joao.p.dourado1@gmail.com>
2025-01-26 12:10:24 +0000

    chore: Script comments changed

--- src/main/java/com/exchangerates/CurrencyExchangeAPI/services/LeakyBucketRateLimiterService.java
+++ src/main/java/com/exchangerates/CurrencyExchangeAPI/services/LeakyBucketRateLimiterService.java
@@ -14,8 +14,8 @@ public class LeakyBucketRateLimiterService implements IRateLimitService {
 
     // TODO: this rate limiter is very coupled to Redis right now, which is not ideal
     private final RedisOperations<String, Object> redisOperations;
-    // 1 key + 3 arguments - time of the request (unix epoch), the leak rate of buckets, and the
-    // maximum capacity
+    // Script inputs: 1 key + 3 arguments - time of the request (unix epoch), the leak rate of buckets, 
+    // and the maximum capacity
     private final RedisScript<Boolean> leakyBucketScript;
 
     private static final int DRAIN_RATE = 1;
